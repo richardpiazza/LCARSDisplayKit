5d01840577b5f159839974d4d4cc1e0506aac88a
Richard Piazza <richard@richardpiazza.com>
2020-11-25 06:44:01 -0600

    Fixed example project.

--- Sources/LCARSDisplayKit/Components/ArcButton.swift
+++ Sources/LCARSDisplayKit/Components/ArcButton.swift
@@ -5,19 +5,19 @@ import UIKit
 /// A Button that draws an arc connected by a right angle pivot.
 open class ArcButton: Button {
     
-    open var arc: Shapes.Arc = Shapes.Arc()
+    open var arc: LCARSDisplayKit.Arc = LCARSDisplayKit.Arc()
     open override var graphable: Graphable {
         get {
             return arc
         }
         set {
-            if let type = newValue as? Shapes.Arc {
+            if let type = newValue as? LCARSDisplayKit.Arc {
                 arc = type
             }
         }
     }
     
-    public convenience init(with arc: Shapes.Arc, inBounds: CGRect, offset: GraphOriginOffset) {
+    public convenience init(with arc: LCARSDisplayKit.Arc, inBounds: CGRect, offset: GraphOriginOffset) {
         let frame = inBounds.frame(graphFrame: arc.graphFrame, offset: offset)
         self.init(frame: frame)
         self.arc = arc
